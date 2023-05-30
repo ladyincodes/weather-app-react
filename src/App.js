8aab33aecefe3130bbc7f68440a51659aa00bca3
Ati <eskafiati@gmail.com>
2023-05-31 11:36:53 +1200

    Added Footer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Weather from "./Weather";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className='App'>
       <Weather />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className='text-center small mt-4 Footer'>
+      <p>
+        <a
+          href='https://github.com/ladyincodes/weather-app-react'
+          target='_blank'
+          rel='noreferrer'
+        >
+          Open-Source Code
+        </a>
+        <span className='author'> by Atefeh Eskafi</span>
+      </p>
+    </footer>
+  );
+}
